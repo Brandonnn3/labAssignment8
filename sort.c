404d3c6a8f8a901f1cf982c9cf8396f7bcc7b84c
Brandonnn3 <106995522+Brandonnn3@users.noreply.github.com>
2024-03-14 23:30:35 -0400

    Adding sort.c and input files along with professor LICENSE

---
+++ sort.c
@@ -0,0 +1,140 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int extraMemoryAllocated;
+
+void *Alloc(size_t sz) {
+    extraMemoryAllocated += sz;
+    size_t* ret = malloc(sizeof(size_t) + sz);
+    *ret = sz;
+    printf("Extra memory allocated, size: %ld bytes\n", sz);
+    return &ret[1];
+}
+
+void DeAlloc(void* ptr) {
+    size_t* pSz = (size_t*)ptr - 1;
+    extraMemoryAllocated -= *pSz;
+    printf("Extra memory deallocated, size: %ld bytes\n", *pSz);
+    free((size_t*)ptr - 1);
+}
+
+void merge(int pData[], int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int *L = (int *)Alloc(n1 * sizeof(int));
+    int *R = (int *)Alloc(n2 * sizeof(int));
+
+    for (i = 0; i < n1; i++)
+        L[i] = pData[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = pData[m + 1 + j];
+
+    i = 0; j = 0; k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            pData[k] = L[i];
+            i++;
+        } else {
+            pData[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        pData[k] = L[i];
+        i++; k++;
+    }
+    
+    while (j < n2) {
+        pData[k] = R[j];
+        j++; k++;
+    }
+
+    DeAlloc(L);
+    DeAlloc(R);
+}
+
+void mergeSort(int pData[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+
+        mergeSort(pData, l, m);
+        mergeSort(pData, m + 1, r);
+        merge(pData, l, m, r);
+    }
+}
+
+int parseData(char *inputFileName, int **ppData) {
+    FILE* inFile = fopen(inputFileName, "r");
+    int dataSz = 0, i, n;
+
+    if (inFile) {
+        fscanf(inFile, "%d\n", &dataSz);
+        *ppData = (int *)Alloc(sizeof(int) * dataSz);
+        if (*ppData == NULL) {
+            printf("Cannot allocate memory\n");
+            exit(-1);
+        }
+        for (i = 0; i < dataSz; ++i) {
+            fscanf(inFile, "%d", &n);
+            (*ppData)[i] = n;
+        }
+        fclose(inFile);
+    }
+    return dataSz;
+}
+
+void printArray(int pData[], int dataSz) {
+    int i, end = dataSz > 100 ? 100 : dataSz;
+    printf("\tData:\n\t");
+    for (i = 0; i < end; ++i) {
+        printf("%d ", pData[i]);
+    }
+    if (dataSz > 100) {
+        printf("\n\t...");
+        for (i = dataSz - 100; i < dataSz; ++i) {
+            printf("%d ", pData[i]);
+        }
+    }
+    printf("\n\n");
+}
+
+int main(void) {
+    clock_t start, end;
+    int i;
+    double cpu_time_used;
+    char* fileNames[] = {"input1.txt", "input2.txt", "input3.txt", "input4.txt"};
+    
+    for (i = 0; i < 4; ++i) {
+        int *pDataSrc, *pDataCopy;
+        int dataSz = parseData(fileNames[i], &pDataSrc);
+        
+        if (dataSz <= 0)
+            continue;
+        
+        pDataCopy = (int *)Alloc(sizeof(int) * dataSz);
+    
+        printf("---------------------------\n");
+        printf("Dataset Size : %d\n", dataSz);
+        printf("---------------------------\n");
+        
+        printf("Merge Sort:\n");
+        memcpy(pDataCopy, pDataSrc, dataSz * sizeof(int));
+        extraMemoryAllocated = 0;
+        start = clock();
+        mergeSort(pDataCopy, 0, dataSz - 1);
+        end = clock();
+        cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+        printf("\truntime\t\t\t: %.2lf seconds\n", cpu_time_used);
+        printf("\textra memory allocated\t: %d bytes\n", extraMemoryAllocated);
+        printArray(pDataCopy, dataSz);
+        
+        DeAlloc(pDataCopy);
+        DeAlloc(pDataSrc);
+    }
+}
